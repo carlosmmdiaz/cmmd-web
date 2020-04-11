c47bafb1a2023736a5287f84d99cb53e7e85cfba
Carlos Munoz <carlosmmdiaz@gmail.com>
2020-04-11 19:29:05 +0200

    feat(card): use borderRadiusSize

--- packages/card/src/CMMDCard.js
+++ packages/card/src/CMMDCard.js
@@ -1,6 +1,6 @@
 import { css, LitElement, html } from 'lit-element';
 
-import { grey, darkgrey } from '@cmmd-web/styles';
+import { grey, darkgrey, borderRadiusSize } from '@cmmd-web/styles';
 
 export class CMMDCard extends LitElement {
   static get styles() {
@@ -8,7 +8,7 @@ export class CMMDCard extends LitElement {
       .card {
         background: ${grey};
         padding: 1rem;
-        border-radius: 0.5rem;
+        border-radius: ${borderRadiusSize};
         box-shadow: 1rem 1rem 1rem -1rem ${darkgrey};
       }
     `;
